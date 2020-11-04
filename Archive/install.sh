91b00c5ec4cc8b1485dad18736dd58b024e57d39
ragegit <maashwin115@gmail.com>
2020-11-04 11:56:51 +0530

    first commit

---
+++ Archive/install.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+namechg(){
+
+	usn=$(ls -t /Users | awk 'NR==1 {print $1}')
+
+	perl -pi -w -e "s/username=.*/username=$usn/g;" RageKiosk/loginInfo/userInformation.ini
+	cp -rf RageKiosk "/Users/$usn/Library/Application Support/"
+	chown -R $usn "/Users/$usn/Library/Application Support/RageKiosk"
+	#chmod 644 "/Users/$usn/Library/Application Support/RageKiosk/log"
+}
+
+inst(){
+	hdiutil mount RageKiosk.dmg
+	cp -rvf "/Volumes/RageKiosk/RageKiosk.app" /Applications/
+}
+
+remve(){
+	hdiutil unmount "/Volumes/RageKiosk/"
+	perl -pi -w -e "s/username=.*/username=/g;" RageKiosk/loginInfo/userInformation.ini
+}
+
+namechg
+#inst
+#remve
